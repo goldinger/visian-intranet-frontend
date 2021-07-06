96fa689c3e79e6ab6b7e21e9cb1d3f93bf5931a2
Amine SGHIR <amine.sghir@external.engie.com>
2021-07-06 10:52:58 +0200

    fix html rendering (newsletter)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Paper from "@material-ui/core/Paper";
+import ReactHtmlParser from 'react-html-parser';
+// eslint-disable-next-line import/no-webpack-loader-syntax
+var __html = require('html-loader!../assets/NEWSLETTER INTERNE - Numéro 11 - Février 2021.html');
 
 
 export default class Home extends React.Component {
   render() {
     return (
         <Paper>
-          <h1>Home</h1>
+            <div>{ ReactHtmlParser(__html) }</div>;
         </Paper>
     );
   }
